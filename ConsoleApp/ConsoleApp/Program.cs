ff88d1944a8f7398631cefbae9c91e6a29fc1f1a
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-11 09:26:56 -0300

    App banco atualizado

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -11,36 +11,73 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             var saldo = 0.00;
-            var valor = 0.00;
-
-            Console.WriteLine("============ BANCO DO JOÃO =====================");
-            Console.WriteLine("1 - DEPOSITAR");
-            Console.WriteLine("2 - SACAR");
-            Console.WriteLine("3 - MOSTRAR SALDO");
-            Console.WriteLine("0 - SAIR");
-            Console.WriteLine("=================================");
-            Console.WriteLine(" **** Informe a opcão desejada: ****");
-            var opcao = Console.ReadLine();
-            switch (opcao)
+            var point = 1;
+            while (point != 0)
             {
-                case "1":
-                    Console.WriteLine("Informe o valor para depositar: ");
-                    valor = Convert.ToDouble(Console.ReadLine());
-                    saldo += valor;
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    Console.WriteLine("O saldo eh de:" + saldo);
-                    break;
-                //case "0":
-                //    Console.WriteLine("Até a proxima!");
-                //    Console.ReadKey();
-                //    break;
-                default:
-                    Console.WriteLine("Opcao invalida!!");
-                    break;
+                Console.WriteLine("============ BANCO DO JOÃO =====================");
+                Console.WriteLine("1 - DEPOSITAR");
+                Console.WriteLine("2 - SACAR");
+                Console.WriteLine("3 - MOSTRAR SALDO");
+                Console.WriteLine("0 - SAIR");
+                Console.WriteLine("=================================");
+                Console.WriteLine(" **** Informe a opcão desejada: ****");
+                var opcao = Console.ReadLine();
+                switch (opcao)
+                {
+                    case "1":
+                        _ = Depositar(ref saldo);
+                        break;
+                    case "2":
+                        _ = Sacar(ref saldo);
+                        break;
+                    case "3":
+                        MostrarSaldo(saldo);
+                        break;
+                    case "0":
+                        point = 0;
+                        Console.WriteLine("Informe qualquer tecla para sair..");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Opcao invalida!!");
+                        break;
+                }
             }
+        }
+
+        private static void MostrarSaldo(double saldo)
+        {
+            Console.WriteLine("O saldo eh de:" + saldo.ToString("C") + "\n");
+            Console.WriteLine("** Digite algo para voltar ao menu **");
+            Console.ReadKey();
+        }
+
+        private static double Sacar(ref double saldo)
+        {
+            double valor;
+            Console.WriteLine("Informe o valor para sacar:");
+            valor = Convert.ToDouble(Console.ReadLine());
+            if (saldo >= valor)
+            {
+                saldo -= valor;
+                Console.WriteLine("Operação realizada com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Limite não disponivel!, voce possui: " + saldo.ToString("C") + "\n");
+            }
+
+            return valor;
+        }
+
+        private static double Depositar(ref double saldo)
+        {
+            double valor;
+            Console.WriteLine("Informe o valor para depositar: ");
+            valor = Convert.ToDouble(Console.ReadLine());
+            saldo += valor;
+            Console.WriteLine("Operação realizada com sucesso!");
+            return valor;
         }
     }
 }
