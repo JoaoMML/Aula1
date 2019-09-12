0b60863f9ea8ff2e8f69c0fe44333e3f412e5dad
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-12 16:43:43 -0300

    Atualizacao e fim dos exercicios while/for

--- LacosDeRepeticao/ProcurandoTextoComWhile/Program.cs
+++ LacosDeRepeticao/ProcurandoTextoComWhile/Program.cs
@@ -1,43 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ProcurandoTextoComFor
+namespace ProcurandoTextoComWhile
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            var palavraComparar = string.Empty;
-            //Console.WriteLine("Informe o texto: ");
-            //var texto = Console.ReadLine();
-            var texto = TextContent();
-            Console.WriteLine("Informe uma palavra para buscar no texto:");
-            var palavra = Console.ReadLine();
-            var count = 0;
+            Console.WriteLine("Informa a palavra de busca:");
+            var wordCount = FindByWhile(Console.ReadLine());
 
-            var tamanhoLimite = texto.Length - (palavra.Length - 1);
-
-            for (int i = 0; i < tamanhoLimite; i++)
-            {
-                palavraComparar = string.Empty;
-                for (int j = 0; j < palavra.Length; j++)
-                {
-                    palavraComparar += texto[i + j].ToString();
-                }
-                if (palavra == palavraComparar)
-                {
-                    count++;
-                }
-
-            }
-            Console.WriteLine($"A palavra '{palavra}' apareceu '{count}' vezes.");
+            Console.WriteLine($"Encontrados:{wordCount}");
             Console.ReadKey();
         }
-
-        /// <summary>
-        /// Texto Fixo
-        /// </summary>
-        /// <returns></returns>
+        private static int FindByWhile(string wordFind)
+        {
+            var contentToFind = TextContent();
+            var lengthText = contentToFind.Length - 2;
+            var contWordFind = 0;
+            int i = 0;
+            while (i < lengthText)
+            {
+                var compareWord = contentToFind[i].ToString() + contentToFind[i + 1].ToString() + contentToFind[i + 2].ToString();
+                if (wordFind == compareWord) contWordFind++;
+                i++;
+            }
+            return contWordFind;
+        }
         private static string TextContent()
         {
             return @"Pensando mais a longo prazo, a execução dos pontos do programa estimula a padronização dos modos de operação convencionais. O empenho em analisar a constante divulgação das informações acarreta um processo de reformulação e modernização dos relacionamentos verticais entre as hierarquias. No entanto, não podemos esquecer que o fenômeno da Internet faz parte de um processo de gerenciamento dos níveis de motivação departamental. A certificação de metodologias que nos auxiliam a lidar com o entendimento das metas propostas apresenta tendências no sentido de aprovar a manutenção do sistema de participação geral. Do mesmo modo, o novo modelo estrutural aqui preconizado garante a contribuição de um grupo importante na determinação do fluxo de informações.
@@ -48,7 +40,7 @@ namespace ProcurandoTextoComFor
 
           É claro que a mobilidade dos capitais internacionais representa uma abertura para a melhoria das condições financeiras e administrativas exigidas. Por outro lado, a valorização de fatores subjetivos desafia a capacidade de equalização das diversas correntes de pensamento. O incentivo ao avanço tecnológico, assim como a contínua expansão de nossa atividade promove a alavancagem das diretrizes de desenvolvimento para o futuro. Ainda assim, existem dúvidas a respeito de como a necessidade de renovação processual causa impacto indireto na reavaliação de todos os recursos funcionais envolvidos.
 
-          Gostaria de enfatizar que a complexidade dos estudos efetuados é uma das consequencias das condições inegavelmente apropriadas. É importante questionar o quanto a crescente influência da mídia ainda não demonstrou convincentemente que vai participar na mudança da gestão inovadora da qual fazemos parte. A nível organizacional, a consolidação das estruturas agrega valor ao estabelecimento dos métodos utilizados na avaliação de resultados. O que temos que ter sempre em mente é que a competitividade nas transações comerciais auxilia a preparação e a composição de alternativas às soluções ortodoxas.
+          Gostaria de enfatizar que a complexidade dos estudos efetuados é uma das consequências das condições inegavelmente apropriadas. É importante questionar o quanto a crescente influência da mídia ainda não demonstrou convincentemente que vai participar na mudança da gestão inovadora da qual fazemos parte. A nível organizacional, a consolidação das estruturas agrega valor ao estabelecimento dos métodos utilizados na avaliação de resultados. O que temos que ter sempre em mente é que a competitividade nas transações comerciais auxilia a preparação e a composição de alternativas às soluções ortodoxas.
 
           Caros amigos, a expansão dos mercados mundiais exige a precisão e a definição dos procedimentos normalmente adotados. Todavia, o surgimento do comércio virtual prepara-nos para enfrentar situações atípicas decorrentes dos conhecimentos estratégicos para atingir a excelência. Acima de tudo, é fundamental ressaltar que a consulta aos diversos militantes possibilita uma melhor visão global do retorno esperado a longo prazo.
 
@@ -60,4 +52,3 @@ namespace ProcurandoTextoComFor
         }
     }
 }
-
