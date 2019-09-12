e74a872b1afe72f776bb13c5a5ccf10b5e0b0cec
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-12 13:43:11 -0300

    Busca de palavra em texto finalizado.

---
+++ LacosDeRepeticao/For/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = "Joao Marcos de Moraes Lima";
+            var count = 0;
+            for (int i = 0; i < nome.Length - 3; i++)
+            {
+                var stringBuilder = $"{nome[i - 1]}-{nome[i]}-{nome[i + 1]}";
+                Console.WriteLine(stringBuilder);
+
+                if (stringBuilder.Contains("o-a"))
+                {
+                    count++;
+                }
+                //Console.ReadKey();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LacosDeRepeticao/UtilizandoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilizandoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teste = 1;
+
+            while (true)
+            {
+                Console.WriteLine(teste);
+                teste--;
+            }
+        }
+    }
+}
