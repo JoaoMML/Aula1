682632009402c3ccda96d7cf789ac5880b6c7907
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-13 15:07:45 -0300

    Ex5 - Finish

---
+++ Trabalho/Ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um texto: ");
+            var qntTextoUsuario = Console.ReadLine().Length;
+            Console.WriteLine($"O texto informado possui: {qntTextoUsuario} caracteres!!");
+            EndMensage();
+        }
+        public static void EndMensage()
+        {
+            Console.WriteLine("Digite qualquer tecla para sair..");
+            Console.ReadKey();
+        }
+    }
+}
