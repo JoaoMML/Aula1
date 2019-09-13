c01044cceeb0c78948142cb69ec105fc701fee81
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-13 14:34:39 -0300

    Ex1 - Finish

---
+++ Trabalho/Ex1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMensage();
+            Console.WriteLine($"\r\n{ TextoContext()}\r\n");
+            EndMensage();
+        }
+
+        public static void WelcomeMensage()
+        {
+            Console.WriteLine("==============================================");
+            Console.WriteLine("============= SEJA BEM VINDO !!===============");
+            Console.WriteLine("==============================================");
+        }
+
+        public static void EndMensage()
+        {
+            Console.WriteLine("Digite qualquer tecla para sair..");
+            Console.ReadKey();
+        }
+
+        private static string TextoContext()
+        {
+            return @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl rhoncus mattis rhoncus urna neque viverra.Eu facilisis sed odio morbi quis commodo odio aenean.Diam vel quam elementum pulvinar etiam. Nunc sed velit dignissim sodales ut. Donec pretium vulputate sapien nec sagittis. Vestibulum mattis ullamcorper velit sed ullamcorper. Etiam sit amet nisl purus in mollis nunc sed id. Elementum facilisis leo vel fringilla est. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. In arcu cursus euismod quis viverra nibh.Velit dignissim sodales ut eu sem integer.Aliquet bibendum enim facilisis gravida neque convallis a. Amet luctus venenatis lectus magna fringilla. Pulvinar neque laoreet suspendisse interdum consectetur.
+
+Placerat vestibulum lectus mauris ultrices.Tristique et egestas quis ipsum suspendisse ultrices.Massa sapien faucibus et molestie ac feugiat.Facilisi cras fermentum odio eu feugiat. In nibh mauris cursus mattis molestie a iaculis. Convallis posuere morbi leo urna molestie at.Facilisis sed odio morbi quis commodo. Leo vel fringilla est ullamcorper eget nulla facilisi etiam.Tortor condimentum lacinia quis vel.Tellus in hac habitasse platea dictumst vestibulum.Quis viverra nibh cras pulvinar mattis. Sit amet venenatis urna cursus eget. Mauris nunc congue nisi vitae.Consectetur adipiscing elit pellentesque habitant.Nunc congue nisi vitae suscipit tellus mauris a diam.Morbi enim nunc faucibus a pellentesque sit amet porttitor.Libero volutpat sed cras ornare.Felis donec et odio pellentesque diam volutpat commodo sed egestas.
+
+Tortor vitae purus faucibus ornare suspendisse. Praesent tristique magna sit amet purus. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Lectus nulla at volutpat diam ut venenatis tellus. Nisl condimentum id venenatis a condimentum vitae sapien. Elementum nibh tellus molestie nunc non blandit massa enim nec. Rutrum tellus pellentesque eu tincidunt tortor. Tempor orci eu lobortis elementum nibh. Varius duis at consectetur lorem.Ac turpis egestas integer eget aliquet nibh.
+
+Nullam eget felis eget nunc lobortis mattis aliquam. Ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Est pellentesque elit ullamcorper dignissim cras tincidunt.Id semper risus in hendrerit gravida rutrum quisque. Nulla aliquet porttitor lacus luctus accumsan. Dui ut ornare lectus sit amet est.At varius vel pharetra vel turpis nunc eget lorem.Adipiscing elit duis tristique sollicitudin.Enim sit amet venenatis urna cursus eget nunc. Turpis egestas maecenas pharetra convallis posuere morbi.Suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Id cursus metus aliquam eleifend mi in nulla.Tempor commodo ullamcorper a lacus.Turpis egestas maecenas pharetra convallis posuere morbi leo urna.Vulputate mi sit amet mauris commodo quis.
+
+Donec adipiscing tristique risus nec feugiat in fermentum posuere. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Donec adipiscing tristique risus nec feugiat in. Neque vitae tempus quam pellentesque nec nam aliquam. Magnis dis parturient montes nascetur.Porta nibh venenatis cras sed.Tortor condimentum lacinia quis vel eros donec ac odio.Nec ullamcorper sit amet risus nullam eget felis. Sed tempus urna et pharetra pharetra massa massa ultricies mi. Eget aliquet nibh praesent tristique magna sit.Purus sit amet volutpat consequat mauris. Eget gravida cum sociis natoque penatibus et magnis. Scelerisque eu ultrices vitae auctor eu. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Enim tortor at auctor urna nunc id.Proin nibh nisl condimentum id venenatis a condimentum vitae.Volutpat odio facilisis mauris sit amet massa vitae. Hac habitasse platea dictumst vestibulum rhoncus.
+
+Velit sed ullamcorper morbi tincidunt ornare massa eget egestas.Lorem dolor sed viverra ipsum nunc aliquet.Eget lorem dolor sed viverra ipsum. Nunc consequat interdum varius sit amet. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor.Non nisi est sit amet facilisis magna etiam tempor.Purus ut faucibus pulvinar elementum integer enim neque. Amet facilisis magna etiam tempor orci eu lobortis. Scelerisque eleifend donec pretium vulputate.Eu non diam phasellus vestibulum lorem sed risus ultricies.Felis donec et odio pellentesque diam. Amet tellus cras adipiscing enim eu turpis egestas. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Odio facilisis mauris sit amet massa. Fringilla est ullamcorper eget nulla facilisi etiam.Curabitur vitae nunc sed velit dignissim sodales ut eu.Leo urna molestie at elementum eu. Vestibulum sed arcu non odio euismod lacinia.Amet nisl purus in mollis nunc. Maecenas sed enim ut sem viverra aliquet eget sit.
+
+Elementum integer enim neque volutpat ac. Varius sit amet mattis vulputate enim. Auctor eu augue ut lectus arcu bibendum at varius vel. Imperdiet sed euismod nisi porta lorem mollis aliquam ut.Amet nulla facilisi morbi tempus iaculis urna id volutpat lacus. Convallis convallis tellus id interdum velit laoreet id donec.Volutpat blandit aliquam etiam erat.Non tellus orci ac auctor augue mauris augue neque gravida. Cras tincidunt lobortis feugiat vivamus at augue eget arcu.Hendrerit dolor magna eget est lorem ipsum dolor sit amet.
+
+Scelerisque eu ultrices vitae auctor eu augue ut lectus.Amet porttitor eget dolor morbi.Sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Sapien nec sagittis aliquam malesuada.Leo duis ut diam quam nulla porttitor.Gravida rutrum quisque non tellus orci ac auctor augue mauris. Viverra mauris in aliquam sem fringilla ut. Nisi est sit amet facilisis magna etiam tempor orci.In iaculis nunc sed augue lacus. Ut tellus elementum sagittis vitae et. Non odio euismod lacinia at.Lorem dolor sed viverra ipsum.Accumsan lacus vel facilisis volutpat est velit egestas. Volutpat maecenas volutpat blandit aliquam etiam erat velit. Posuere urna nec tincidunt praesent semper feugiat.Convallis a cras semper auctor neque vitae tempus quam pellentesque. Tortor aliquam nulla facilisi cras fermentum odio eu.
+
+Elit pellentesque habitant morbi tristique senectus et netus et.Urna condimentum mattis pellentesque id nibh. Egestas purus viverra accumsan in nisl nisi scelerisque eu. Ornare quam viverra orci sagittis eu volutpat odio facilisis.Egestas erat imperdiet sed euismod.Morbi tristique senectus et netus et malesuada fames. Ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel. Placerat vestibulum lectus mauris ultrices eros in cursus turpis. Amet risus nullam eget felis eget. Adipiscing vitae proin sagittis nisl rhoncus mattis.Adipiscing commodo elit at imperdiet dui accumsan sit amet.Purus in massa tempor nec.Dolor purus non enim praesent.Lobortis feugiat vivamus at augue eget arcu dictum varius.Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat.Et malesuada fames ac turpis egestas integer eget aliquet.
+
+Mi in nulla posuere sollicitudin aliquam ultrices sagittis. Sollicitudin tempor id eu nisl nunc. Erat pellentesque adipiscing commodo elit.Posuere urna nec tincidunt praesent semper. Tellus elementum sagittis vitae et leo duis.Vitae congue mauris rhoncus aenean vel elit scelerisque. Convallis tellus id interdum velit laoreet. Et egestas quis ipsum suspendisse ultrices gravida dictum. Gravida cum sociis natoque penatibus et magnis.Vitae semper quis lectus nulla.Libero nunc consequat interdum varius sit amet mattis. Sit amet justo donec enim diam. At lectus urna duis convallis convallis tellus.Tempus urna et pharetra pharetra massa massa ultricies mi quis. Mi bibendum neque egestas congue.Risus feugiat in ante metus dictum at tempor commodo. Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Tempor id eu nisl nunc mi ipsum faucibus vitae.Egestas sed sed risus pretium quam.";
+        }
+    }
+}
