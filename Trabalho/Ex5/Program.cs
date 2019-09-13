dad14c6356b510bfda7589e923e952c017d9883c
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-13 15:18:49 -0300

    Ex5 - Finish

---
+++ Trabalho/Ex5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe uma palavra");
+            var texto = Console.ReadLine();
+            Console.WriteLine($" Primeira: {texto[0]}\n Ultima:{texto[texto.Length - 1].ToString()}");
+            Console.ReadKey();
+        }
+    }
+}
