d4bc58699a00e287402363d9b6b44ed0c83b731f
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-13 15:31:38 -0300

    Ex6 - Finish

---
+++ Trabalho/Ex6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Informe um texto:");
+            var text = Console.ReadLine();
+            Console.WriteLine("==============================================================");
+            Console.WriteLine($" O texto original: {text} ");
+            Console.WriteLine("==============================================================");
+            Console.WriteLine($" O texto replaced: {text.Replace("Banana", "Laranja")} ");
+            Console.WriteLine("\n Digite algo para finalizar..");
+            Console.ReadKey();
+        }
+    }
+}
