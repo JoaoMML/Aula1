fca8e136ecf4703e5503d2990a0653b32978bf67
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-20 10:33:28 -0300

    Minha forma

---
+++ SolucaoParaLocacaoDeLivros/InterfaceBiblioteca/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocacaoBiblioteca.Controller;
+
+namespace InterfaceBiblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BoasVindas();
+            ValidaAcesso();
+            Console.ReadKey();
+        }
+
+        private static void BoasVindas()
+        {
+            Console.Clear();
+            Console.WriteLine("================================");
+            Console.WriteLine("SISTEMA DE LOCAÇÃO DE LIVROS");
+            Console.WriteLine("================================");
+            Console.WriteLine("Para acessar informe seu login e senha!!");
+        }
+        private static void ValidaAcesso()
+        {
+            Console.WriteLine("Login:");
+            var login = Console.ReadLine();
+
+            Console.WriteLine("Senha:");
+            var senha = Console.ReadLine();
+
+            UsuarioController usuarioController = new UsuarioController();
+
+            if (usuarioController.LoginSistema(login, senha))
+            {
+                Console.Clear();
+                Console.WriteLine($"Seja bem vindo {login}!");
+                MenuSistema();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("O usuario ou senha não existe, tente novamente!");
+                ValidaAcesso();
+            }
+        }
+        private static void MenuSistema()
+        {
+            Console.WriteLine("** Escolha a opção desejada **");
+            Console.WriteLine("0 - Sair ");
+        }
+    }
+}
---
+++ SolucaoParaLocacaoDeLivros/LocacaoBiblioteca/Controller/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocacaoBiblioteca.Controller
+{
+    public class UsuarioController
+    {
+        /// <summary>
+        /// Metodo que realiza o login dentro do nosso sistema
+        /// Para realizar o login padrão use:
+        /// - Login: Admin
+        /// - Senha Admin
+        /// </summary>
+        /// <param name="login">Login do usuário dentro do sistema</param>
+        /// <param name="senha">Senha do usuário dentro do sistema</param>
+        /// <returns>Retorna verdadeiro se existir um usuario com login e senha</returns>
+        public bool LoginSistema(string login, string senha)
+        {
+            if (login == "Admin" && senha == "Admin")
+            {
+                return true;
+            }
+            {
+                return false;
+            }
+        }
+    }
+}
