2e0a8edfddb61c57b3cebaff6da04853f0cac77f
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-20 10:49:19 -0300

    Melhoras no sistema com uso de um controlador de fluxo

--- SolucaoParaLocacaoDeLivros/InterfaceBiblioteca/Program.cs
+++ SolucaoParaLocacaoDeLivros/InterfaceBiblioteca/Program.cs
@@ -12,19 +12,16 @@ namespace InterfaceBiblioteca
         static void Main(string[] args)
         {
             BoasVindas();
-            ValidaAcesso();
+            ControladorDeFluxo();
+            MenuSistema();
             Console.ReadKey();
         }
 
-        private static void BoasVindas()
-        {
-            Console.Clear();
-            Console.WriteLine("================================");
-            Console.WriteLine("SISTEMA DE LOCAÇÃO DE LIVROS");
-            Console.WriteLine("================================");
-            Console.WriteLine("Para acessar informe seu login e senha!!");
-        }
-        private static void ValidaAcesso()
+        /// <summary>
+        /// Verifica o usuario e senha e retorna um bollean para usar no ControladorDeFluxo 
+        /// </summary>
+        /// <returns></returns>
+        private static bool ValidaAcesso()
         {
             Console.WriteLine("Login:");
             var login = Console.ReadLine();
@@ -34,23 +31,55 @@ namespace InterfaceBiblioteca
 
             UsuarioController usuarioController = new UsuarioController();
 
-            if (usuarioController.LoginSistema(login, senha))
+            return usuarioController.LoginSistema(login, senha);
+
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine($"Seja bem vindo {login}!");
+            //    MenuSistema();
+            //}
+            //else
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine("O usuario ou senha não existe, tente novamente!");
+            //    ValidaAcesso();
+            //}
+
+
+        }
+
+        /// <summary>
+        ///Fazendo um controlador de fluxo (Melhor que funções recursivas!);
+        /// </summary>
+        private static void ControladorDeFluxo()
+        {
+            while (!ValidaAcesso())
             {
                 Console.Clear();
-                Console.WriteLine($"Seja bem vindo {login}!");
-                MenuSistema();
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("O usuario ou senha não existe, tente novamente!");
-                ValidaAcesso();
+                Console.WriteLine("Login ou senha inválidos!");
             }
         }
+
+        /// <summary>
+        /// Menu do sistema
+        /// </summary>
         private static void MenuSistema()
         {
+            Console.Clear();
             Console.WriteLine("** Escolha a opção desejada **");
             Console.WriteLine("0 - Sair ");
+        }
+
+        /// <summary>
+        /// Mensagem de Inicialização!
+        /// </summary>
+        private static void BoasVindas()
+        {
+            Console.Clear();
+            Console.WriteLine("================================");
+            Console.WriteLine("SISTEMA DE LOCAÇÃO DE LIVROS");
+            Console.WriteLine("================================");
+            Console.WriteLine("Para acessar informe seu login e senha!!");
         }
     }
 }
