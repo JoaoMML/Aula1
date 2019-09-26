b87422a3584079d5bb10b9a8e685e9edc87998e5
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-26 15:08:04 -0300

    Fim

---
+++ FuncoesLinq/exer25-09/Controller/CarrosController.cs
@@ -0,0 +1,22 @@
+﻿using exer25_09.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exer25_09.Controller
+{
+    public class CarrosController
+    {
+        SistemaVendasContext sistemaContext = new SistemaVendasContext();
+
+        public List<CarroModel> GetCarros(int mes = 0)
+        {
+            if (mes == 0)
+                return sistemaContext.ListaPublica;
+            else
+                return sistemaContext.ListaPublica.FindAll(x => x.Ano.Month == mes);
+        }
+    }
+}
