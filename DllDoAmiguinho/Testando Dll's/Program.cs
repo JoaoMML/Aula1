d44cf22fbe7bf5d5524452e5cfc4d8d8eba4934d
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-09-27 17:23:22 -0300

    commit

---
+++ DllDoAmiguinho/Testando Dll's/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DllPastaWin;
+
+namespace Testando_Dll_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentsAutoGenerate documentsAutoGenerate = new DocumentsAutoGenerate();
+
+            documentsAutoGenerate.DeletarPastaMeusDocumentos("PastaWan", true);
+            documentsAutoGenerate.ObterPastasDiretorioMeuDocumentos().ForEach(i => Console.WriteLine(i));
+            //documentsAutoGenerate.CriarPastaMeusDocumentos("PastaWan");
+            Console.ReadLine();
+        }
+    }
+}
