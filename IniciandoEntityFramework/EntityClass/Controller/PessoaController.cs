8bc434495a80e3dad6dc476738c1950b1bdb90dd
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-10-07 15:05:26 -0300

    Iniciando Entity Funcking framework

---
+++ IniciandoEntityFramework/EntityClass/Controller/PessoaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Colocamos o using da pasta Model
+using EntityClass.Model;
+
+namespace EntityClass.Controller
+{
+    //Definimos a class como public
+    public class PessoaController
+    {
+        //Inicializamos o EntituContextDB
+        EntityContextDB contextDb = new EntityContextDB();
+
+        public IQueryable<Pessoa> GetPessoas()
+        {
+            return contextDb.ListaDePessoas;
+        }
+
+        public void AddPessoa(Pessoa item)
+        {
+            contextDb.ListaDePessoas.Add(item);
+            contextDb.SaveChanges();
+        }
+    }
+}
---
+++ IniciandoEntityFramework/EntityClass/Model/EntityContextDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityClass.Model
+{
+    // Aqui herdamos da class DbContext
+    public class EntityContextDB : DbContext
+    {
+        // DbSet é uma palavra reservad
+        public DbSet<Pessoa> ListaDePessoas { get; set; }
+    }
+}
