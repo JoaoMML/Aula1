4ac77deaf431dcb7bdaeec8a3f96a50e583d052c
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-10-09 15:40:26 -0300

    Celulares

---
+++ SistemaCelular/CatalogoCelulares/Controller/CelularesController.cs
@@ -0,0 +1,65 @@
+﻿using CatalogoCelulares.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogoCelulares.Controller
+{
+    public class CelularesController
+    {
+        CelularesContextDB contextDb = new CelularesContextDB();
+
+        //Listagem
+        public IQueryable<Celular> GetCelulares()
+        {
+            return contextDb.Celulares.Where(x => x.Ativo == true);
+        }
+
+        //Atualizacao
+        public bool AtualizaCelular(Celular item)
+        {
+            var celular = contextDb.Celulares.FirstOrDefault(x => x.Id == item.Id);
+
+            if (celular == null)
+                return false;
+            else
+                celular = item;
+            contextDb.SaveChanges();
+
+            return true;
+        }
+
+        // Inserção
+        public bool InserirCelular(Celular item)
+        {
+            //Fazemos as validacoes ao contrario pois se algum item não corresponder ao necessário ele já sai sem testar os proximos!
+            if (string.IsNullOrWhiteSpace(item.Marca))
+                return false;
+            if (string.IsNullOrWhiteSpace(item.Modelo))
+                return false;
+            if (item.Preco <= 0)
+                return false;
+
+            // Se passar todos ele adiciona no comando logo abaixo!
+            contextDb.Celulares.Add(item);
+            contextDb.SaveChanges();
+            return true;
+        }
+
+        //Remoção (** Deixar inativo! **)
+        public bool RemoverCelular(int id)
+        {
+            var celular = contextDb.Celulares.FirstOrDefault(x => x.Id == id);
+
+            if (celular == null)
+                return false;
+
+            celular.Ativo = false;
+            contextDb.SaveChanges();
+
+            return true;
+        }
+    }
+}
---
+++ SistemaCelular/CellDu/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogoCelulares.Controller;
+using CatalogoCelulares.Model;
+
+namespace CellDu
+{
+    class Program
+    {
+        static CelularesController celulares = new CelularesController();
+        static void Main(string[] args)
+        {
+            //Definimos uma opção de menu inicial
+            var opcao = int.MinValue;
+            //Definimos que enquanto for diferente de 0 
+            //Ele continua em nosso loop
+            while (opcao != 0)
+            {
+                //Apresentação do menu inicial
+                Console.WriteLine("Escolha um menu");
+                Console.WriteLine("1 - Inserir Celular");
+                Console.WriteLine("0 - Sair");
+                //Obtemos o menu escolhido pelo usuario
+                opcao = int.Parse(Console.ReadLine());
+                //switch nos ajuda com a escolha da opção
+                switch (opcao)
+                {
+                    //Caso for opção 1 ele inseri celular
+                    case 1:
+                        InserirCelular();
+                        break;
+                    default://Casso colocar uma opção não valida 
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+                //Colocamos true para informar que não deve aparecer na 
+                //tela o valor pressionado
+                Console.WriteLine("Pressione qualquer tecla para voltar ao menu inicial.");
+                Console.ReadKey(true);
+                Console.Clear();
+
+            }
+        }
+
+        public static void InserirCelular()
+        {
+            Console.WriteLine("_*_* INSERIR CELULAR _*_*");
+            Console.WriteLine("Informe a marca");
+            var marca = Console.ReadLine();
+            Console.WriteLine("Informe o modelo:");
+            var modelo = Console.ReadLine();
+            Console.WriteLine("Informe o preço:");
+            var preco = double.Parse(Console.ReadLine());
+
+            var resultado = celulares.InserirCelular(new Celular()
+            {
+                Marca = marca,
+                Modelo = modelo,
+                Preco = preco
+            });
+        }
+    }
+}
