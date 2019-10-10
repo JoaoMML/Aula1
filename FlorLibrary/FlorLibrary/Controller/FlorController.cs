a50bea707e3657c4491b4745dd542f59a805e93b
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-10-10 17:14:17 -0300

    exercici

---
+++ FlorLibrary/FlorLibrary/Controller/FlorController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlorLibrary.Model;
+
+namespace FlorLibrary.Controller
+{
+    public class FlorController
+    {
+        FlorContext contextDb = new FlorContext();
+
+        public IQueryable<Flor> GetFlores()
+        {
+            return contextDb.Flores.Where(x => x.Ativo == true);
+        }
+
+        //Atualizacao
+        public bool Atualiza(Flor item)
+        {
+            var celular = contextDb.Flores.FirstOrDefault(x => x.Id == item.Id);
+
+            if (celular == null)
+                return false;
+            else
+                celular = item;
+            contextDb.SaveChanges();
+
+            return true;
+        }
+
+        // Inserção
+        public bool Inserir(Flor item)
+        {
+            //Fazemos as validacoes ao contrario pois se algum item não corresponder ao necessário ele já sai sem testar os proximos!
+            if (string.IsNullOrWhiteSpace(item.Marca))
+                return false;
+            if (string.IsNullOrWhiteSpace(item.Modelo))
+                return false;
+            if (item.Preco <= 0)
+                return false;
+
+            // Se passar todos ele adiciona no comando logo abaixo!
+            contextDb.Flores.Add(item);
+            contextDb.SaveChanges();
+            return true;
+        }
+
+        //Remoção (** Deixar inativo! **)
+        public bool Remover(int id)
+        {
+            var celular = contextDb.Flores.FirstOrDefault(x => x.Id == id);
+
+            if (celular == null)
+                return false;
+
+            celular.Ativo = false;
+            contextDb.SaveChanges();
+
+            return true;
+        }
+    }
+}
---
+++ FlorLibrary/FlorLibrary/Model/Flor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlorLibrary.Model
+{
+    public class Flor : Controle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(30), Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public int Quantidade { get; set; }
+    }
+}
