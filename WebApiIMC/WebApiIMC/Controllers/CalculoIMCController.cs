8c3343235061e66c2062b6be19fa6c67f7599ffe
João Marcos <JoaoMarcos_rgt@hotmail.com>
2019-10-28 15:10:21 -0300

    stage 2 - exercicio 2

--- WebApiIMC/WebApiIMC/Controllers/CalculoIMCController.cs
+++ WebApiIMC/WebApiIMC/Controllers/CalculoIMCController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebApiIMC.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class CalculoIMCController : ApiController
     {
         public string Get()
